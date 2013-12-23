75dee3a391f1afdbf968fe2a96d61ce5a5318be8
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-12-23 11:56:05 +0100

    send and receive json

--- public/js/shorten.js
+++ public/js/shorten.js
@@ -2,29 +2,30 @@
     var PostRequest = function(url){
 	var http = new XMLHttpRequest();
 	http.open('POST', url, true);
-	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.setRequestHeader("Content-type", "application/json");
 	http.setRequestHeader("Connection", "close")
 	this.http = http;
     }
-    PostRequest.prototype.send = function(params, callback){
+    PostRequest.prototype.send = function(parameters, callback){
+	var json = JSON.stringify(parameters);
 	var http = this.http;
 	callback = callback || function(){};
-	http.setRequestHeader("Content-length", params.length);
+	http.setRequestHeader("Content-length", json.length);
 	http.onreadystatechange = function(){
 	    if (http.readyState == 4 && http.status == 200) {
 		callback.call(http.responseText);
 	    }
 	}
-	http.send(params);
+	http.send(json);
     }
 
     var input = document.getElementById('target');
 
     function shorten(){
 	var target = input.value;
-	var params = "target=" + target;
+	var parameters = { 'target': target };
 
-	new PostRequest(window.location.origin).send(params, function(){
+	new PostRequest(window.location.origin).send(parameters, function(){
 	    console.log('posted');
 	});
     }

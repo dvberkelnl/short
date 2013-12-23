c24b72a10b7e627cd508fc90eb224ec4de94222e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-12-23 08:40:25 +0100

    retrieve target value

--- public/js/shorten.js
+++ public/js/shorten.js
@@ -1,6 +1,8 @@
 (function(){
+    var input = document.getElementById('target');
+
     var button = document.getElementById('shorten');
     button.addEventListener('click', function(){
-	console.log('button clicked');
+	console.log(input.value);
     });
 })();

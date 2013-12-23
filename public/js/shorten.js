343c62fafdf50edf54f15d9f724ce14ccfa7126c
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-12-23 09:32:13 +0100

    refactor posting

--- public/js/shorten.js
+++ public/js/shorten.js
@@ -1,21 +1,32 @@
 (function(){
+    var PostRequest = function(url){
+	var http = new XMLHttpRequest();
+	http.open('POST', url, true);
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.setRequestHeader("Connection", "close")
+	this.http = http;
+    }
+    PostRequest.prototype.send = function(params, callback){
+	var http = this.http;
+	callback = callback || function(){};
+	http.setRequestHeader("Content-length", params.length);
+	http.onreadystatechange = function(){
+	    if (http.readyState == 4 && http.status == 200) {
+		callback.call(http.responseText);
+	    }
+	}
+	http.send(params);
+    }
+
     var input = document.getElementById('target');
 
     function shorten(){
 	var target = input.value;
 	var params = "target=" + target;
 
-	var http = new XMLHttpRequest();
-	http.open('POST', window.location.origin, true);
-	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	http.setRequestHeader("Content-length", params.length);
-	http.setRequestHeader("Connection", "close")
-	http.onreadystatechange = function(){
-	    if (http.readyState == 4 && http.status == 200) {
-		console.log('posted');
-	    }
-	}
-	http.send(params);
+	new PostRequest(window.location.origin).send(params, function(){
+	    console.log('posted');
+	});
     }
 
     var button = document.getElementById('shorten');

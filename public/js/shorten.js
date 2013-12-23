65802be6b44e5f3bb7edafba757541deb4898aaa
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-12-23 08:53:44 +0100

    post target to server

--- public/js/shorten.js
+++ public/js/shorten.js
@@ -1,8 +1,23 @@
 (function(){
     var input = document.getElementById('target');
 
+    function shorten(){
+	var target = input.value;
+	var params = "target=" + target;
+
+	var http = new XMLHttpRequest();
+	http.open('POST', window.location.origin, true);
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.setRequestHeader("Content-length", params.length);
+	http.setRequestHeader("Connection", "close")
+	http.onreadystatechange = function(){
+	    if (http.readyState == 4 && http.status == 200) {
+		console.log('posted');
+	    }
+	}
+	http.send(params);
+    }
+
     var button = document.getElementById('shorten');
-    button.addEventListener('click', function(){
-	console.log(input.value);
-    });
+    button.addEventListener('click', shorten);
 })();
--- server.js
+++ server.js
@@ -3,10 +3,15 @@ var app = express();
 var server = require('http').createServer(app);
 
 app.set('port', process.env.PORT || 3435);
+app.use(express.bodyParser());
 app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', function(request, response){
     response.send(200, "Hello World");
+});
+app.post('/', function(request, response){
+    console.log(request.body);
+    response.send(200);
 });
 
 server.listen(app.get('port'));

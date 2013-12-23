13d1dda61f4eb4f59b2e8fc9cd4538eaffa179bb
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-12-23 09:25:26 +0100

    refactor shortening

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
+var shorten = require('./lib/routing/shorten');
+
 app.set('port', process.env.PORT || 3435);
 app.use(express.bodyParser());
 app.use('/static', express.static(__dirname + '/public'));
@@ -9,10 +11,7 @@ app.use('/static', express.static(__dirname + '/public'));
 app.get('/', function(request, response){
     response.send(200, "Hello World");
 });
-app.post('/', function(request, response){
-    console.log(request.body);
-    response.send(200);
-});
+app.post('/', shorten);
 
 server.listen(app.get('port'));
 console.log('listening on port ' + app.get('port'));

5510a0175a9406e377552f5091668e4de73ffc4a
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-12-23 11:20:43 +0100

    specify notation

---
+++ test/notation_test.js
@@ -0,0 +1,38 @@
+var expect = require('chai').expect;
+
+var factory = require('../lib/notation');
+
+describe('notation', function(){
+    var notations = {
+	'abcdefghijklmnopqrstuvwxyz' : {
+	    0 : 'a',
+	    1 : 'b',
+	    26: 'aa'
+	},
+	'abc' : {
+	    0  : 'a',
+	    1  : 'b',
+	    2  : 'c',
+	    3  : 'aa',
+	    4  : 'ab',
+	    5  : 'ac',
+	    6  : 'ba',
+	    7  : 'bb',
+	    8  : 'bc',
+	    9  : 'ca',
+	    10 : 'cb',
+	    11 : 'cc',
+	    12 : 'aaa'
+	}
+    }
+    for (digits in notations) {
+	var notation = factory(digits);
+
+	var cases = notations[digits];
+	for (input in cases) {
+	    it('should convert a \'' + input + '\' to \'' + cases[input] + '\'', function(){
+		expect(notation(input)).to.equal(cases[input]);
+	    });
+	}
+    };
+});

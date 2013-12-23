42f25317408bcefa5427d28a1adc73655f27d011
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-12-23 08:35:54 +0100

    server static content

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var server = require('http').createServer(app);
 
 app.set('port', process.env.PORT || 3435);
+app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', function(request, response){
     response.send(200, "Hello World");

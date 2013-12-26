7ac2b00e958febc20e2730109c429f6c6a70209a
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-12-26 07:53:42 +0100

    redirect '/' to '/static'
    fixes #1

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 
-var shortener = require('./lib/routing/shorten')();
+var Redis = require('./lib/repository/redis');
+
+var shortener = require('./lib/routing/shorten')({
+    repository: new Redis({}, { port: '6379' })
+});
+var redirecter = require('./lib/routing/redirecter');
 
 app.set('port', process.env.PORT || 3435);
 app.use(express.json());
 app.use('/static', express.static(__dirname + '/public'));
 
 app.post('/', shortener.shorten);
+app.get('/', redirecter.root);
 app.get('/:key', shortener.redirect);
 
 server.listen(app.get('port'));
